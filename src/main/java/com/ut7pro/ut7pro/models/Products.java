1680c6a7f1ee2081bb3b36962b55be95f93ebcf4
Trifunski <72448185+Trifunski@users.noreply.github.com>
2023-06-06 00:25:13 +0100

    Add files via upload

---
+++ src/main/java/com/ut7pro/ut7pro/models/Products.java
@@ -0,0 +1,70 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ut7pro.ut7pro.models;
+
+import jakarta.persistence.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "Productos")
+@EntityListeners(AuditingEntityListener.class)
+public class Products {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "descripcion")
+    private String descripcion;
+    @Column(name = "precio")
+    private long precio;
+
+    public Products() {
+
+    }
+
+    public Products(long id, String nombre, String descripcion, long precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+    }
+
+    // Getters y setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getnombre() {
+        return nombre;
+    }
+
+    public void setnombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getdescripcion() {
+        return descripcion;
+    }
+
+    public void setdescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public long getprecio() {
+        return precio;
+    }
+
+    public void setprecio(long precio) {
+        this.precio = precio;
+    }
+}
