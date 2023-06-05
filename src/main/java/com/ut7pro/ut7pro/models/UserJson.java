72dc49e9c40c07e64c39286a7f8598fb09cc0aea
Trifunski <72448185+Trifunski@users.noreply.github.com>
2023-06-05 23:35:22 +0100

    Add files via upload

---
+++ src/main/java/com/ut7pro/ut7pro/models/UserJson.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ut7pro.ut7pro.models;
+
+public class UserJson {
+
+    private int id;
+    private String name;
+    private String email;
+    private int edad;
+    private String rol;
+
+    public UserJson() {
+    }
+
+    public UserJson(int id, String name, String email, int edad, String rol) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.edad = edad;
+        this.rol = rol;
+    }
+
+    // Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
